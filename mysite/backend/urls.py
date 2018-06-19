b24092667d45ecedecf4aa2d77921ee31226c8b9
Shivam Chamoli <dhruvchamoli@gmail.com>
2018-06-19 15:35:30 -0400

    returning comma separated partners

--- mysite/backend/urls.py
+++ mysite/backend/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    # ex: /partner/5/
+    path('partners', views.returnAllPartners,name='returnAllPartners'),
+    path('partner/<int:partner_id>/', views.getPartner, name='getPartner'),
+    # ex: /polls/5/results/
+    #path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/backend/views.py
+++ mysite/backend/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import urls
+from .models import Partner
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hi")
+
+#def partners(request):
+#    var partners = []
+
+def returnAllPartners(request):
+    partner_list = Partner.objects.all()
+    output = ', '.join([p.partner_name for p in partner_list])
+    return HttpResponse(output)
+
+def getPartner(request, partner_id):
+    return HttpResponse("You're looking at partner %s." % partner_id)
     
